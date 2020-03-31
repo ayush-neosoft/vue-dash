18647c2074bb923bf8d30bd3fb8903a328eb7a2c
Ayush Likhar <ayush.likhar@outlook.com>
2020-03-31 11:27:17 +0530

    init

---
+++ src/pages/Dashboard/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="md-layout text-center">
+    Welcome
+  </div>
+</template>
+<script>
+// import { LoginCard } from "@/components";
+export default {
+  components: {
+    // LoginCard
+  },
+  data() {
+    return {
+      // firstname: null,
+      // email: null,
+      // password: null
+    };
+  }
+};
+</script>
+
+<style></style>
